59fe8d9a8df940ac72d13274a537ba9a19cfd04f
Saksham Garg <saksham.garg14509@gmail.com>
2024-04-13 22:44:50 +0530

    n
    n

--- models/university/university.js
+++ models/university/university.js
@@ -5,18 +5,17 @@ const School = require('./school');
 const universitySchema = new Schema({
     Name: { type: String, required: true },
     Email: { type: String, required: true },
-    Logo: { type: String },
+    Logo: {
+        url: String,
+        filename: String,
+    },
     // Address: {
     //     Address_Line_1: { type: String, required: true },
     //     Address_Line_2: { type: String },
     //     City: { type: String, required: true },
     //     State_Province: { type: String, required: true },
     //     Country: { type: String, required: true },
-    //     Postal_Code: { type: String, required: true }
-    // },
-    // Contact: {
-    //     Email: { type: String, required: true },
-    //     Phone_Number: { type: String, required: true }
+    //     Postal_Code: { type: Number, required: true }
     // },
     // Schools: [{ type: Schema.Types.ObjectId, ref: 'School' }] 
 });
